a912670a2dae261c35d09260b2813562379510fa
Danilo Valério <danvalerio89@gmail.com>
2026-07-15 11:24:11 -0300

    chore: config main

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
-import { Provider } from "@/components/ui/provider.tsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "@/App";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/queryClient";
+import { AppChakraProvider } from "@/theme/provider";
+import { AppToaster } from "@/components/ui/toaster";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <AppChakraProvider>
+        <App />
+        <AppToaster />
+      </AppChakraProvider>
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    </QueryClientProvider>
   </StrictMode>,
 );
